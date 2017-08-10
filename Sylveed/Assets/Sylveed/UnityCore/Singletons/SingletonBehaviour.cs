7a779efda5a6106265b482e2bb34cfdbd2b00595
yuu tajimi <ytajmi@samaa.co.jp>
2017-08-10 21:47:25 +0900

    setup unity project

---
+++ Sylveed/Assets/Sylveed/UnityCore/Singletons/SingletonBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Sylveed
+{
+	public class SingletonBehaviour<T> : InternalSingletonBehaviour<T>
+		where T : SingletonBehaviour<T>
+	{
+		public static new T Instance
+		{
+			get { return InternalSingletonBehaviour<T>.Instance; }
+		}
+	}
+}
---
+++ Sylveed/Assets/Sylveed/UnityCore/Singletons/SingletonBehaviourNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Sylveed
+{
+	public class SingletonBehaviourNotFoundException : Exception
+	{
+		public SingletonBehaviourNotFoundException(Type type)
+			: base(string.Format("シングルトン({0})が見つかりません。", type.Name))
+		{
+
+		}
+	}
+}
